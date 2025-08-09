54dff9598343afe483023dccd556fbbc51e1587e
mokoko <guswlso2@naver.com>
2025-08-09 17:18:12 +0900

    css: 메인page, footer css 수정

--- app/components/home/footer/Footer.tsx
+++ app/components/home/footer/Footer.tsx
@@ -5,30 +5,35 @@ import GithubIcon from '@/components/icons/GithubIcon';
 
 export default function Footer() {
   return (
-    <footer className='flex gap-[24px] flex-wrap items-center justify-center h-[60px]'>
-      <a
-        href='naver.com'
-        className='flex gap-2 hover:underline hover:underline-offset-4'
-      >
-        <GithubIcon width={24} height={24} />
-        깃허브
-      </a>
-      <a
-        href='https://www.q-net.or.kr/'
-        className='flex gap-2 hover:underline hover:underline-offset-4'
-      >
-        <Image
-          aria-hidden
-          src='/qnet.svg'
-          alt='qnet icon'
-          width={24}
-          height={24}
-        />
-        큐넷
-      </a>
-      <Suspense fallback={<p>수정 시간 로딩중...</p>}>
-        <ModifiedTime />
-      </Suspense>
+    <footer className='flex gap-4 sm:gap-12 flex-wrap justify-center items-center mt-10'>
+      <div className='flex gap-2 sm:gap-8'>
+        <a
+          href='https://github.com/ahrrrl/ISQ_QuizGenerator'
+          className='flex gap-2 hover:underline hover:underline-offset-4'
+        >
+          <GithubIcon width={24} height={24} />
+          깃허브
+        </a>
+        <a
+          href='https://www.q-net.or.kr/'
+          className='flex gap-2 hover:underline hover:underline-offset-4'
+        >
+          <Image
+            aria-hidden
+            src='/qnet.svg'
+            alt='qnet icon'
+            width={24}
+            height={24}
+          />
+          큐넷
+        </a>
+      </div>
+
+      <div>
+        <Suspense fallback={<p>수정 시간 로딩중...</p>}>
+          <ModifiedTime />
+        </Suspense>
+      </div>
     </footer>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import KeyPoints from './components/home/keyPoints/KeyPoints';
 
 export default async function Home() {
   return (
-    <div className='min-h-screen flex flex-col p-8 sm:px-20 sm:py-8'>
-      <main className='flex-1 flex flex-col items-center justify-center gap-12'>
+    <div className='min-h-screen flex flex-col items-center p-8 sm:px-20 sm:py-8 '>
+      <main className='flex-1 max-w-[1024px] flex flex-col items-center justify-around gap-12 mt-14'>
         <HomeTitle />
-        <KeyPoints />
         <CategoryList />
+        <KeyPoints />
       </main>
       <div className=''>
         <Footer />
